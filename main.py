9e915f346285f9aeba1d1c01822a5c620b75c2c4
JessieHuang123 <baobao88yu@gmail.com>
2024-08-20 21:13:41 +0900

    Implemented output functionality

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ ligand_pdb = "Caffeine.pdb" # Caffeine PDB file
 # Load structures using MDAnalysis
 protein_u = mda.Universe(protein_pdb)
 ligand_u = mda.Universe(ligand_pdb)
+
+# Select protein and ligand
+protein = protein_u.select_atoms("protein")
+ligand = ligand_u.select_atoms("all")  # Select all atoms of caffeine
+
+# Move caffeine to the center of the protein (manual adjustment may be needed in this case)
+ligand.translate(protein.center_of_mass() - ligand.center_of_mass())
+
+# Calculate distances between protein and caffeine
+distances_result = distances.distance_array(protein.positions, ligand.positions)
