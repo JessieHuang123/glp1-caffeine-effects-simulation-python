ab6fa1e81ac6a7e04f76c07a6b463295d77afc4f
JessieHuang123 <baobao88yu@gmail.com>
2024-08-27 21:22:44 +0900

    Conducted partial testing

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import sys
 import MDAnalysis as mda
 from MDAnalysis.analysis import distances
 import nglview as nv
 import matplotlib.pyplot as plt
 
-# PDB file paths
-protein_pdb = "7ki0.pdb"   # Protein PDB file
-ligand_pdb = "Caffeine.pdb" # Caffeine PDB file
+# Check if the correct number of arguments is provided
+if len(sys.argv) != 3:
+    print("Usage: python script.py <protein_pdb> <ligand_pdb>")
+    sys.exit(1)
+
+# Get the PDB file paths from command-line arguments
+protein_pdb = sys.argv[1]
+ligand_pdb = sys.argv[2]
 
 # Load structures using MDAnalysis
 protein_u = mda.Universe(protein_pdb)
@@ -13,19 +19,19 @@ ligand_u = mda.Universe(ligand_pdb)
 
 # Select protein and ligand
 protein = protein_u.select_atoms("protein")
-ligand = ligand_u.select_atoms("all")  # Select all atoms of caffeine
+ligand = ligand_u.select_atoms("all")  # Select all atoms of ligand
 
-# Move caffeine to the center of the protein (manual adjustment may be needed in this case)
+# Move ligand to the center of the protein (manual adjustment may be needed in this case)
 ligand.translate(protein.center_of_mass() - ligand.center_of_mass())
 
-# Calculate distances between protein and caffeine
+# Calculate distances between protein and ligand
 distances_result = distances.distance_array(protein.positions, ligand.positions)
 
 # Visualization: Display molecular structures using NGLView
 view = nv.show_mdanalysis(protein_u)
 view.add_representation("cartoon", selection="protein")
-view.add_component(ligand_u)  # Add caffeine
-view.add_representation("ball+stick", component=1)  # Display caffeine in ball and stick style
+view.add_component(ligand_u)  # Add ligand
+view.add_representation("ball+stick", component=1)  # Display ligand in ball and stick style
 view
 
 # Calculate and visualize minimum distances
@@ -33,7 +39,7 @@ min_distances = distances_result.min(axis=1)
 plt.hist(min_distances, bins=50)
 plt.xlabel("Distance (Å)")
 plt.ylabel("Frequency")
-plt.title("Distance Distribution between Protein and Caffeine")
+plt.title("Distance Distribution between Protein and Ligand")
 plt.show()
 
 # Print results
